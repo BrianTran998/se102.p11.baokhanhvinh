8b6c787890fb17c94f1beeaccedaf6feb7e685cc
giabao98 <trangiabao98@gmail.com>
2024-11-15 00:20:13 +0700

    feat: Relocate folder structure

--- BlasterMaster/main.cpp
+++ BlasterMaster/main.cpp
@@ -201,7 +201,7 @@ int WINAPI WinMain(
 
 
 	//IMPORTANT: this is the only place where a hardcoded file name is allowed ! 
-	game->Load(L"Config\\SceneConfig.txt");  
+	game->Load(L"SceneConfig.txt");  
 
 	SetWindowPos(hWnd, 0, 0, 0, SCREEN_WIDTH*2, SCREEN_HEIGHT*2, SWP_NOMOVE | SWP_NOOWNERZORDER | SWP_NOZORDER);
 
